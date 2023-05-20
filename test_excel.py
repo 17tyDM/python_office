ad0184e90939888c2abaf7b4465afa49c677f7c8
17tyDM <takehiiragi@gmail.com>
2023-05-20 16:10:38 +0900

    python_excel_practice

--- test_excel.py
+++ test_excel.py
@@ -16,3 +16,11 @@ ws['A2'] = datetime.datetime.now()
 
 # Save the file
 wb.save("sample.xlsx")
+
+ws2 = wb.create_sheet('practice')
+num = 0
+for i in range(1,11):
+    for j in ['A','B','C','D','E','F','G','H','I','J']:
+        num += 1
+        ws2[f"{j}{i}"] = num
+wb.save("sample.xlsx")
